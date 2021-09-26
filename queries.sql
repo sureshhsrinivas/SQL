f10ac679687611e6107bd0f08bef4c75dd694b39
sureshhsrinivas <90232438+sureshhsrinivas@users.noreply.github.com>
2021-09-25 23:20:30 -0700

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -24,7 +24,7 @@ select * from orderitem;
 -- 8. List all the product with product id 100
 select productname from product where pid=100;
 
--- 9. List all the info of product with name Vivo V3
+-- 9. List all the info of product with name S5
 select * from product where productname='S5';
 
 -- 10. List pid, productname, price of the phone named "Galaxy Grand"
@@ -67,14 +67,14 @@ select * from customer Where customername LIKE'A%';
 select * from customer where customername Like '%i'; 
 
 -- 21. List all the customer whos name starts with 'R' and ends with 'i'
-select * from customer where customername like 'R%' and customername like '%i';
+select * from customer where customername like 'R%i';
 
 
 -- 22. List all the orders for the year 2013
 select * from orderd where orderdate like '2013%';
 
 -- 23. List all the products which are not from the category 10001
-select * from product where categoryid!=10001;
+select * from product where categoryid not in (10001);
 
 -- 24. List all the products which are not from the category 10001
 -- or 10003 with the price more than 30000 and product name starts
@@ -157,22 +157,12 @@ select pid from product where productname='6S'
 
 
 -- 33. List all the customers who have ordered for Apple I-Pad Mini from bangalore
-select * from customer where customerid in
-(
-select customerid from orderd where orderid IN
-(
-select orderid from orderitem where pid=
-(
-select pid from product where productname='4S' or city = 'Bangalore'
-)
-)
-); 
-
 select * from customer c inner join customeraddress ca
 on ca.customerid=c.customerid inner join
 orderd o on c.customerid=o.customerid
 inner join orderitem oi on o.orderid=oi.orderid
 having oi.pid=(select pid from product where productname='4S')and ca.city='bangalore';
+
 
 -- 34. List all the phones which Goutham orderd for
 select * from product where pid in
@@ -192,9 +182,9 @@ select * from product where pid in
 (
 select pid from orderitem where orderid IN
 (
-select orderid from orderd where customerid =
+select orderid from orderd where customerid in
 (
-select customerid from customer where customername='Amrutha' or orderdate like '2014%'
+select customerid from customer where customername in('Amrutha' or orderdate like '2014%')
 )
 )
 );
@@ -205,6 +195,10 @@ select * from product where pid in
 (
 select pid from 
 );
+
+select distinct c.customername,c.customerid,o.orderid from orderd o
+right join customer c on
+c.customerid=o.customerid;
 
 
 -- 37. List Fav phones of Bangalorean
@@ -222,7 +216,7 @@ select * from product where pid IN
 (
 select pid from orderitem where orderid IN
 (
-select orderid from orderd where orderdate IN('2013-01-01',2013-12-31)
+select orderid from orderd where orderdate between '2013-01-01'and '2013-12-31'
 )
 );
 
@@ -260,96 +254,77 @@ c.customerid=ca.customerid;
 
 -- 43. List customer name and customer city of all the customer
 -- who have never bought any product
-select c.customername,ca.city from customer c
-inner join customeraddress ca on
-c.customerid=ca.customerid having not in ;
-
+select c.customername , ca.city from customer c inner join customeraddress ca on c.customerid=ca.customerid;
 
 -- 44. List Customer id, customer name, orderdate, of all the orders
-select c.customerid,c.customername,o.orderdate from customer c
-inner join orderd o on
-c.customerid=o.customerid;
-
+select c.customerid,c.customername,o.orderdate from customer c inner join orderd o on c.customerid=o.customerid;
 
 -- 45. List Customer id, customer name, orderdate, company and
 -- product name with qty, price of all the orders
-select c.customerid,c.customername,o.orderdate,co.companyname,p.productname,oi.qty,p.price from customer c,
-full join orderd o on 
-
+select c.customerid , c.customername ,o.orderdate,co.companyname,p.productname,p.price,oi.qty
+from customer c inner join orderd o on c.customerid =o.customerid inner join orderitem oi on o.orderid=oi.orderid 
+inner join product p on oi.pid=p.pid inner join company co on p.companyid=co.companyid;
 
 -- 46. List Customer id, customer name, orderdate, company and
 -- product name with qty, price and amount of all the orders
 -- where amount is qty*price
+select c.customerid,c.customername,o.orderdate,co.companyname,p.productname,oi.qty,p.price,(oi.qty*p.price) as amount from
+customer c inner join orderd o on c.customerid=o.customerid inner join orderitem oi on o.orderid=oi.orderid inner join product p
+on oi.pid=p.pid inner join company co on p.companyid=co.companyid;
 
-
-
-/* 47. List Customer id, customer name, orderdate, company and
+-- 47. List Customer id, customer name, orderdate, company and
 product name with qty, price and amount of all the orders
-where amount > 50,000 (amount is qty*price) */
+where amount > 50,000 (amount is qty*price) --
+select c.customerid,c.customername,o.orderdate,co.companyname,p.productname,oi.qty,p.price,(oi.qty*p.price) as amount from
+customer c inner join orderd o on c.customerid=o.customerid inner join orderitem oi on o.orderid=oi.orderid inner join product p
+on oi.pid=p.pid inner join company co on p.companyid=co.companyid having amount>50000;
 
-
-
-
-/* 48. List customerid, customername, city, companyname, productname,
-qty, price and amount of all the products orderd */
-
-
-
-
-/* 49. List all product, company, customer and
+-- 48. List customerid, customername, city, companyname, productname,
+qty, price and amount of all the products orderd --
+select c.customerid,c.customername,ca.city,co.companyname,p.productname,oi.qty,p.price,(oi.qty*p.price) as amount from
+ customer c inner join customeraddress ca on c.customerid=ca.customerid inner join orderd o on ca.customerid=o.customerid
+ inner join orderitem oi on o.orderid=oi.orderid inner join product p on p.pid=oi.pid inner join company co on co.companyid=p.companyid;
+ 
+-- 49. List all product, company, customer and
 customer address details for all orders
-which were ordered in the year 2014
-*/
+which were ordered in the year 2014 --
+select  p.productname,co.companyname,c.customername,ca.stName,ca.city,ca.state,o.orderdate from customer c inner join customeraddress ca 
+on c.customerid=ca.customerid inner join orderd o on o.customerid=ca.customerid inner join orderitem oi on oi.orderid=o.orderid 
+inner join product p on p.pid=oi.pid inner join company co on co.companyid=p.companyid having o.orderdate between '2014-01-01'and '2014-12-31';
 
+-- 50. Update amount of order item --
+update product set price=20000  where productname='Galaxy Grand';
+select productname,price from product where productname='Galaxy Grand'; 
 
+-- 51. Get the total sales based on orderid --
+select count(orderid) as total_sales from orderd;
 
+-- 52. Get the total sales based on given month --
+select month(orderdate),count(orderid) as total_sales from orderd where month(orderdate)='02';
 
-/* 50. Update amount of order item */
+-- 53. Get the total sales based on year --
+select year(orderdate),count(orderid) as total_sales from orderd where year(orderdate)='2013' ;
 
+-- 54. Get the total sales based on month and year --
+select year(orderdate),month(orderdate),count(orderid) as total_sales from orderd;
 
+-- 55. Total sales based on product --
 
-/* 51. Get the total sales based on orderid */
+-- 56. Total sales based on company --
+select companyname ,count(companyid) as total_sales from company;
 
+-- 57. Display top 3 sold mobiles --
+select p.productname, sum(price) from product p group by productname order by sum(price) desc limit 3; 
 
+-- 58. Top 3 customers based on billing amount -- 
 
-/* 52. Get the total sales based on given month */
+-- 59. Top 3rd customer based on billing amount --
 
+-- 60. Display all unique phones which are sold --
+ select DISTINCT(p.productname) from product p;
+ 
+-- 61. Display all phones which are sold with the no. of quantity --
+select p.productname,oi.qty from orderitem oi inner join product p on p.pid=oi.pid ;
 
-
-/* 53. Get the total sales based on year */
-
-
-
-/* 54. Get the total sales based on month and year */
-
-
-
-/* 55. Total sales based on product */
-
-
-
-/* 56. Total sales based on company */
-
-
-
-/* 57. Display top 3 sold mobiles */
-
-
-
-/* 58. Top 3 customers based on billing amount */
-
-
-
-/* 59. Top 3rd customer based on billing amount */
-
-
-
-/* 60. Display all unique phones which are sold */
-
-
-
-/* 61. Display all phones which are sold with the no. of quantity */
-
-
-
-/* 62. Name of the top priced phone */
+-- 62. Name of the top priced phone --
+select max(price) as top_priced,productname from product;
